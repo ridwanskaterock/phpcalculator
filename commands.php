659571c162fb6d5102839dacbad38fc1b5318b8e
ridwan <ridwanskaterocks@gmail.com>
2019-11-15 16:56:48 +0700

    Register multiply command

--- commands.php
+++ commands.php
@@ -3,4 +3,5 @@
 return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubstractCommand::class,
+    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
 ];
