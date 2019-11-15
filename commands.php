fc0cbad8047c39bd0b057f6821ad8e28e8685b88
ridwan <ridwanskaterocks@gmail.com>
2019-11-15 17:48:48 +0700

    Add divide command

--- commands.php
+++ commands.php
@@ -4,4 +4,5 @@ return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubstractCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
+    \Jakmall\Recruitment\Calculator\Commands\DivideCommand::class,
 ];
